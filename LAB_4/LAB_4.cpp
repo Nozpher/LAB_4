411bc1de00b12b43ab65bf99312ebcab27977056
Nozpher <66372864+Nozpher@users.noreply.github.com>
2022-08-08 16:49:01 +0500

    Добавьте файлы проекта.

---
+++ LAB_4/LAB_4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+
+
+using namespace std;
+
+int main()
+{
+    int mas[100];
+    int leight, b, c, smesh;
+    for (int i = 0; i < 100; i++) {
+        mas[i] = rand() % 100;
+    }
+    for (int h = 0; h < 100; h++) {
+        cout << mas[h] << " ";
+    }
+
+    cout << "Write massive leight: ";
+    cin >> leight;
+    cout << endl<< "What number u need remove? >> " << endl;
+    cin >> b;
+    for (int i = 0; i < leight; i++)
+    {
+        if (mas[i] == b)
+        {
+            for(long c = i; c < i; ++i)
+            {
+                mas[c] = mas[c + 1];
+            }
+            
+         }
+
+    }
+    cout << "Write leight of offset:";
+    cin >> smesh;
+    for (int l = 0; l < smesh; l++) {
+        mas[l] = mas[l + 1];
+    }
+    for (int h = 0; h < leight; h++) {
+        cout << mas[h] << " ";
+    }
+}
